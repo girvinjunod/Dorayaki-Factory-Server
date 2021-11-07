0690948e69f99a23e2234ec759768a212abcac37
girvinjunod <girvinjunod@gmail.com>
2021-11-07 19:31:09 +0700

    feat: add jwt token

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ const cors = require('cors')
 const app = express()
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken')
 
 app.use(cors())
 app.use(express.json());
@@ -22,11 +23,30 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+const verifyJWT = (req, res, next) => {
+  const token = req.headers["x-access-token"]
+
+  if (!token){
+    res.send("no token")
+  } else{
+    jwt.verify(token, "pisangjerukanjing", (err, decode) => {
+      if (err){
+        res.send({auth: false, err: "Failed to verify token"})
+      } else{
+        console.log("verified")
+        next()
+      }
+
+    })
+  }
+}
+
 app.post('/login', (req, res) => {
   console.log(req.body)
   var uname = req.body.uname
   var pass = req.body.password
-  connection.connect()
+  // connection.connect()
   connection.query('SELECT * from user where username=?', [ uname ], 
   function (err, rows) {
     if (err) res.send({err: err})
@@ -35,13 +55,19 @@ app.post('/login', (req, res) => {
       bcrypt.compare(pass, rows[0].password, function(err, result) {
         console.log(result)
         if (result){
-          res.send(result)
+          const id = rows[0].id
+          const token = jwt.sign({id}, "pisangjerukanjing", {
+            expiresIn: 300
+          })
+          console.log(id)
+
+          res.send({auth: true, token:token, result: result})
         } else{
-          res.send({err: "password doesn't match"})
+          res.send({auth: false, err: "password doesn't match"})
         }
       });
     } else{
-      res.send({err: "user doesn't exist"})
+      res.send({auth: false, err: "user doesn't exist"})
     }
   })
   // connection.end()
